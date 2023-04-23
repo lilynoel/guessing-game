69f8eb3c7ff0449b4af14093299735ba4bed0870
lilynoel <lilydnas@gmail.com>
2023-04-23 19:21:21 +1000

    button functionality added for submit and reset

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,29 @@
+// constant variables linking to buttons in HTML page
+const submitButton = document.getElementById("submitButton");
+const resetButton = document.getElementById("resetButton");
+
 // variable to store number of guesses
-let guesses = 0
+let guesses = 0;
 
 // function to generate a random number
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * max + min);
+  return Math.floor(Math.random() * max + min);
 }
 
 // variable to hold random number generator with chosen parameters
-let randomNumber = generateRandomNumber(1, 20)
+let randomNumber = generateRandomNumber(1, 20);
 
-// check to see if it works: it does :-) 
-console.log(randomNumber)
+// check to see if it works: it does :-)
+console.log(randomNumber);
+
+// event for submit button
+submitButton.addEventListener('click', () => {
+    console.log("You clicked the submit button");
+})
+
+
+// event for reset button
+resetButton.addEventListener('click', () => {
+    console.log("You clicked the reset button");
+});
 
