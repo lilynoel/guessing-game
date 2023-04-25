346d0f62bc8d3541b1139b6aa5887849567afa14
lilynoel <lilydnas@gmail.com>
2023-04-25 17:34:27 +1000

    submit button functionality with console logs

--- javascript/index.js
+++ javascript/index.js
@@ -17,13 +17,35 @@ let randomNumber = generateRandomNumber(1, 20);
 console.log(randomNumber);
 
 // event for submit button
-submitButton.addEventListener('click', () => {
-    console.log("You clicked the submit button");
-})
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
+  console.log("You clicked the submit button");
 
-// event for reset button
-resetButton.addEventListener('click', () => {
-    console.log("You clicked the reset button");
+  // capture input somehow
+  let userGuess = document.getElementById("guess").value;
+
+  // increase number of guesses
+  guesses++;
+
+  if (userGuess == randomNumber) {
+    // check if guess is correct
+    // if correct, end game and display win message
+    console.log("That's Correct! :-D");
+    submitButton.disabled = true;
+
+    // if not correct, check if number of guesses is > 3
+  } else if (guesses > 3) {
+    // if number of guesses is > 3: end game & display lose message
+    submitButton.disabled = true;
+    console.log("Game Over!");
+
+  } else {
+    console.log("Not Correct! :-(");
+  }
 });
 
+// event for reset button
+resetButton.addEventListener("click", () => {
+  console.log("You clicked the reset button");
+});
